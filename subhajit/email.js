013aa367077ffcb74052cf56415081fd6e0dd32a
Subhajit <subhajit28061997@gmail.com>
2020-03-12 20:59:10 +0530

    proper indentation

--- subhajit/email.js
+++ subhajit/email.js
@@ -1,31 +1,31 @@
-exports.helpmail=(to1,from1,subject1,text1)=>{
+exports.helpmail = (from1, to1, subject1, text1) => {
 
-var mailhelper=require('nodemailer');
+	var mailhelper = require('nodemailer');
 
-var  smail=mailhelper.createTransport({
-	service:'gmail',
-	auth: {
-		 user :'subhajit28061997@gmail.com',
-		  pass:'taghzwdztpnrmiwa'
+	var smail = mailhelper.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'subhajit28061997@gmail.com',
+			pass: 'taghzwdztpnrmiwa'
+		}
+	});
+
+
+	var message = {
+		from: from1,
+		to: to1,
+		subject: subject1,
+		text: text1
 	}
-});
 
 
-var  message={
-	from:to1,
-	to:from1,
-	subject: subject1,
-  text: text1
-}
-
-
-smail.sendMail(message, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-}); 
+	smail.sendMail(message, function (error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Email sent: ' + info.response);
+		}
+	});
 }
 
 
--- subhajit/router.js
+++ subhajit/router.js
@@ -1,20 +1,20 @@
-var exp=require('express');
+var exp = require('express');
 
-var router= exp.Router();
-var  mail=require('./email');
+var router = exp.Router();
+var mail = require('./email');
 
 
 
-router.get('/',function(req,res){
-       var from='subhajit28061997@gmail.com';
-       var to='subhajit.nandi@joshsoftware.com';
+router.get('/', function (req, res) {
+       var from = 'subhajit28061997@gmail.com';
+       var to = 'subhajit.nandi@joshsoftware.com';
        var subject = 'mail test';
-       var text='first mail';
-	mail.helpmail(from,to,subject,text);
-	
-	res.json({
-		
-		'success': 'mail sended'
-	})
+       var text = 'first mail';
+       mail.helpmail(from, to, subject, text);
+
+       res.json({
+
+              'success': 'mail sended'
+       })
 })
-module.exports=router;
+module.exports = router;
