f7c73627d27af9e3a487a968fb92e905c9949902
Jitu007-Bunde <jitendrasbunde@gmail.com>
2020-03-13 18:53:43 +0530

    Notification Api

--- emailApi/index.js
+++ emailApi/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const email = require('./emailApi')
 const app = express();
+const notification = require('./notification.js');
 
 app.use('/email',email);
+app.use('/notification',notification);
 app.listen(3007,function(){
 	console.log('Server is running on Port 3007');
 }
--- emailApi/notification.js
+++ emailApi/notification.js
@@ -6,16 +6,16 @@ router.get('/',function(req,res){
 	res.json({'url':'default get call in emailApi.js'});
 });
 
-router.post('/send',function(req,res){
-	var obj = {
+router.get('/send/:email-:cc-:text',function(req,res){
+
+  var obj = {
 		from :'jitendrasbunde@gmail.com',
-		to :'sunil.sonawane@joshsoftware.com,jitendrasbunde@gmail.com',
-		subject  :'Email API test',
+		to :req.params.email+","+req.params.cc,
+		subject  :'mail by'+req.params.email,
 	 	text :'Node Mail API Testing mail By Jitendra'
   }
- email.mailSend(obj);
+  email.mailSend(obj);
  		res.json({'url':'post  call in emailApi.js'});
  }
  );
 module.exports = router;
-
