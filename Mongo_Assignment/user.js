002cec4e8b037a99eca92c6c98da5202789d68b4
Manish21-v <vitkarmanish21@gmail.com>
2020-03-15 23:42:41 +0530

    small changes made as per the changes done in the mongo_connection file

--- Mongo_Assignment/user.js
+++ Mongo_Assignment/user.js
@@ -7,7 +7,7 @@ const myDbConnection = require('./mongo_Connection');
 
 //Getting the user details as json body
 router.post('/add',function (req,res) {
-  console.log(`${req.body}`);
+  //console.log(`${req.body}`);
 
   //Adding the user details json object into the userdetail table
   myDbConnection.getUser().collection('userdetail').insertOne(req.body, function(err, res) 
@@ -17,21 +17,8 @@ router.post('/add',function (req,res) {
       console.log(err);
     }
     console.log("1 document inserted");
-    
   });
-
   
-  // myDbConnection.close();
-  //cannot insert the data into the table after closing the connection !!!
-  // myDbConnection.getUser().collection('userdetail').insertOne(req.body, function(err, res) 
-  // {    
-  //   if (err) 
-  //   { //if inserting data fails
-  //     console.log(err);
-  //   }
-  //   console.log("1 document inserted twice in database");
-    
-  // });
   res.json(`json object sended`);
 })
 
