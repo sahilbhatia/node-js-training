bcb9d373401c7ed0ed2e980d34736e19a0e73306
Manish21-v <vitkarmanish21@gmail.com>
2020-03-16 12:48:56 +0530

    changed the name of the router to sendnotification and given camel case to the variable

--- email_api/email.js
+++ email_api/email.js
@@ -5,32 +5,32 @@ const sendEmail = require('./send_email');
 
 //Sending email using path parameters !!!
 //http://localhost:3003/email/credentials/vitkarmanish21@gmail.com/vitkarmanish21@gmail.com/Testing%20api/Did%20you%20receive%20email
-router.get('/credentials/:from/:to/:subject/:text',function(req,res,next)
+router.get('/sendnotification/:from/:to/:subject/:text',function(req,res,next)
 {
   console.log(`${req.params}`);
  // console.log(`${req.params.from}`);
 
-  var mail_Obj = {
+  var mailObj = {
     from : req.params.from,
     to : req.params.to,
     subject : req.params.subject,
     text : req.params.text
   }
-  sendEmail.emailData(mail_Obj);
+  sendEmail.emailData(mailObj);
   res.json("mail has been sended via path parameter");
 })
 
 //sending mail through request parameters!!!
-router.get('/credentials',function(req,res,next)
+router.get('/sendnotification',function(req,res,next)
 {
   console.log(req.query);
-  var mail_Obj = {
+  var mailObj = {
     from : req.query.from,
     to : req.query.to,
     subject : req.query.subject,
     text : req.query.text
   }
-  sendEmail.emailData(mail_Obj);
+  sendEmail.emailData(mailObj);
   res.json("mail has been sended via request parameter");
 })
 
