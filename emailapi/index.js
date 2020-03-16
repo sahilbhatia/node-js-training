1777f407218d07036e012d02fdc87a13ccdd9b9e
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-03-16 14:27:48 +0530

    delete folder

--- emailapi/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-var notification= require('./sendnotification.js');
-
-app.use('/email', notification);
-
-app.listen(3000);
-console.log('server at 3000');
