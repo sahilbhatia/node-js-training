3b1b89f0863d8a15f6d5eeb0b80f6dbffeec239c
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-03-16 14:33:06 +0530

    updated crud

---
+++ mongodconn/mailApi.js
@@ -0,0 +1,23 @@
+const nodeMailer = require('nodemailer');
+const routingFile= require('./userRouter.js');
+
+function senderFunction(mailOptions){
+	var transporter = nodeMailer.createTransport({
+	service: 'gmail',
+	auth: {	
+		user: 'onkarhasabe1@gmail.com',
+		pass: 'popatraohasabe'
+	}
+	});
+transporter.sendMail(mailOptions, function(error, info){
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Email sent: ' + info.response);
+	}
+	});
+};
+ 
+module.exports = {
+	senderFunction
+}
