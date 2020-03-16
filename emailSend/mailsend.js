5547f7b13cd83e87d921bc41cd161dc861d35d3d
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-03-16 14:46:53 +0530

    basic email code

---
+++ emailSend/mailsend.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const nodeMailer = require('nodemailer');
+const file= require('./sendNotification.js');
+
+function senderFunction(mailOptions){
+	var transporter = nodeMailer.createTransport({
+	service: 'gmail',
+	auth: {	
+		user: 'onkarhasabe1@gmail.com',
+		pass: '*********'
+	}
+	});
+transporter.sendMail(mailOptions, function(error, info){
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Email sent: ' + info.response);
+	}
+	});
+};
+ 
+module.exports = {
+	senderFunction
+}
+
