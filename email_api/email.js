7c7d7a48993a0135aa83df9b91b5bf087c823b66
Manish21-v <vitkarmanish21@gmail.com>
2020-03-16 12:56:13 +0530

    removed the unnecessary comment lines

--- email_api/email.js
+++ email_api/email.js
@@ -4,7 +4,6 @@ nodeMailer = require('nodemailer');
 const sendEmail = require('./send_email');
 
 //Sending email using path parameters !!!
-//http://localhost:3003/email/credentials/vitkarmanish21@gmail.com/vitkarmanish21@gmail.com/Testing%20api/Did%20you%20receive%20email
 router.get('/sendnotification/:from/:to/:subject/:text',function(req,res,next)
 {
   console.log(`${req.params}`);
