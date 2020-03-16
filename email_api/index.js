0fafb37bd46f8aa6534aab3dc3a952c0123a94ac
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-03-16 12:02:35 +0530

    email_api_assignment

---
+++ email_api/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const notificationFile= require('./sendNotification.js');
+
+app.use('/email', notificationFile);
+
+app.listen(3000);
+console.log('server at 3000');
+
