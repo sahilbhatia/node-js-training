da4cf68526ecfc30ca064e2cbc2c270c473fd6cb
Manish21-v <vitkarmanish21@gmail.com>
2020-03-19 13:27:50 +0530

    Added environment variable to mongo configuration file

--- MongoUsingMongoose/config.js
+++ MongoUsingMongoose/config.js
@@ -1,3 +1,7 @@
+const url = process.env.URL;
+const port = process.env.PORT;
+const dbName = process.env.NAME;
+
 module.exports = {
-  url: 'mongodb://localhost:27017/user'
+  url: `mongodb://${url}:${port}/${dbName}`
 }
