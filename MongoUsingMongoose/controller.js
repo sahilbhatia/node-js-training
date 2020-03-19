6d1bfe730bec33f13787a97908c11a0680e261fe
Manish21-v <vitkarmanish21@gmail.com>
2020-03-19 12:51:55 +0530

    Added all the code for user crud using Mongoose ODM

---
+++ MongoUsingMongoose/controller.js
@@ -0,0 +1,121 @@
+const User = require('./userModel.js');
+// Create and Save a new User
+exports.create = (req, res) => {
+  // Validate request
+  if(!req.body.email) {
+      return res.status(400).send({
+          message: "User email-id can not be empty"
+      });
+  }
+
+  // Create a User
+  const user = new User({
+      id : req.body.id || "Untitled User", 
+      name : req.body.name,
+      email : req.body.email,
+      location :req.body.location
+  });
+
+  // Save User in the database
+  user.save()
+  .then(data => {
+      res.send(data);
+  }).catch(err => {
+      res.status(500).send({
+          message: err.message || "Some error occurred while creating the User."
+      });
+  });
+};
+
+// Retrieve and return all user from the database.
+exports.findAll = (req, res) => {
+  User.find()
+  .then(user => {
+      res.send(user);
+  }).catch(err => {
+      res.status(500).send({
+          message: err.message || "Some error occurred while retrieving users."
+      });
+  });
+};
+
+// Find a single note with a noteId
+exports.findOne = (req, res) => {
+  console.log(req.params.email);
+  User.findOne({email :req.params.email})
+  .then(user => {
+      if(!user) {
+          return res.status(404).send({
+              message: "User not found with email " + req.params.email
+          });            
+      }
+      res.send(user);
+  }).catch(err => {
+      if(err.kind === 'ObjectId') {
+          return res.status(404).send({
+              message: "User not found with email " + req.params.email
+          });                
+      }
+      return res.status(500).send({
+          message: "Error retrieving user with email " + req.params.email
+      });
+  });
+};
+
+// Update a note identified by the noteId in the request
+exports.update = (req, res) => {
+  // Validate Request
+  if(!req.body.email) {
+      return res.status(400).send({
+          message: "User email can not be empty"
+      });
+  }
+
+  // Find note and update it with the request body
+  User.findOneAndUpdate(req.params.email, {
+      id : req.body.id || "Untitled user",
+      name : req.body.name,
+      email : req.body.email,
+      location :req.body.location
+  }, {new: true})
+  .then(user => {
+      if(!user) {
+          return res.status(404).send({
+              message: "User not found with email " + req.params.email
+          });
+      }
+      res.send(user);
+  }).catch(err => {
+      if(err.kind === 'ObjectId') {
+          return res.status(404).send({
+              message: "User not found with email " + req.params.email
+          });                
+      }
+      return res.status(500).send({
+          message: "Error updating user with email " + req.params.email
+      });
+  });
+};
+
+// Delete a user with the specified email in the request
+exports.delete = (req, res) => {
+  User.findOneAndDelete(req.params.email)
+  .then(user => {
+      if(!user) {
+          return res.status(404).send({
+              message: "User not found with email" + req.params.email
+          });
+      }
+      res.send({message: "User deleted successfully!"});
+  }).catch(err => {
+      if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+          return res.status(404).send({
+              message: "User not found with email " + req.params.email
+          });                
+      }
+      return res.status(500).send({
+          message: "Could not delete user with email " + req.params.email
+      });
+  });
+};
+
---
+++ MongoUsingMongoose/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    id : Number,
+    name : String,
+    email : {
+      type : String, unique :true
+    },
+    location : String
+}, {
+    timestamps: true
+});
+//1)param - modelname, 2)param -schemaname ,3)param - collectionName
+module.exports = mongoose.model('User', userSchema,'userlist');
