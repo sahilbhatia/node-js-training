16c21424c05c86f524d3a268a639afe6393d7c7f
Manish21-v <vitkarmanish21@gmail.com>
2020-03-23 19:33:55 +0530

    Added httpServer

--- MongoUsingMongoose/server.js
+++ MongoUsingMongoose/server.js
@@ -4,15 +4,8 @@ const bodyParser = require('body-parser');
 const dbConfig = require('./config.js');
 // create express app
 const app = express();
-//var fs = require('fs');
-//var https = require('https');
-//var privateKey  = fs.readFileSync('./key.pem', 'utf8');
-//var certificate = fs.readFileSync('./cert.pem', 'utf8');
-//var credentials = {key: privateKey, cert: certificate};
 var http = require('http');
 var httpServer = http.createServer(app);
-//var httpsServer = https.createServer(credentials, app);
-
 // parse requests of content-type 
 app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
@@ -22,5 +15,4 @@ const routerObj = require('./routes');
 app.use('/',routerObj);
 //connecting to the database !!!
 dbConfig.connect();
-httpServer.listen(8080);
-//httpsServer.listen(8443);
+httpServer.listen(8443);
