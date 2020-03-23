4150fa240348bc646c1f5138357d65b3c212386d
Manish21-v <vitkarmanish21@gmail.com>
2020-03-23 19:33:08 +0530

    Added router for setpassword,checkuser

--- MongoUsingMongoose/routes.js
+++ MongoUsingMongoose/routes.js
@@ -1,28 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const user = require('./controller');
-
-const { validate, ValidationError, Joi } = require('express-validation');
-
-const loginValidation = {
-  body: Joi.object({
-    id : Joi.number(),
-    name : Joi.string(),
-    email: Joi.string()
-      .email()
-      .required(),
-      location : Joi.string()
-  }),
-}
+//const { validate, ValidationError, Joi } = require('express-validation');
+const { check, validationResult } = require('express-validator');
+// const loginValidation = {
+//   body: Joi.object({
+//     id : Joi.number(),
+//     name : Joi.string(),
+//     email: Joi.string()
+//       .email(),
+//       location : Joi.string(),
+//       password : Joi.string()
+//   }),
+// }
 
 //Add user in the Database
-router.post('/user',validate(loginValidation),user.create);
+router.post('/user',[
+	check('email').isEmail()
+	],/*validate(loginValidation),*/user.create);
+//check username and password
+router.put('/user/checkuser/:email',user.checkUser);
 
  // Retrieve all user
-
  router.get('/user', user.findAll);
 
- // Retrieve a single User with serEmail
+ // Retrieve a single User with userEmail
  router.get('/user/:email', user.findOne);
 
  // Update a user with userEmail
@@ -30,11 +32,14 @@ router.post('/user',validate(loginValidation),user.create);
 
  // Delete a user with userEmail
  router.delete('/user/:email', user.delete);
+
+ //Set password with userEmail
+ router.put('/user/setpassword/:email',user.updatePassword);
+
  router.use(function(err, req, res, next) {
   if (err instanceof ValidationError) {
     return res.status(err.statusCode).json(err)
   }
- 
   return res.status(500).json(err)
 })
 module.exports = router;
