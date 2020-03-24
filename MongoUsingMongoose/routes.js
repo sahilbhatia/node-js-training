197a22b2825ca1b3f778ce661ac3a77f7aeefb20
Manish21-v <vitkarmanish21@gmail.com>
2020-03-24 22:10:22 +0530

    Added routers for creating jwt token and autheticating the token and changed the name of some routes

--- MongoUsingMongoose/routes.js
+++ MongoUsingMongoose/routes.js
@@ -3,38 +3,27 @@ const router = express.Router();
 const user = require('./controller');
 //const { validate, ValidationError, Joi } = require('express-validation');
 const { check, validationResult } = require('express-validator');
-// const loginValidation = {
-//   body: Joi.object({
-//     id : Joi.number(),
-//     name : Joi.string(),
-//     email: Joi.string()
-//       .email(),
-//       location : Joi.string(),
-//       password : Joi.string()
-//   }),
-// }
-
 //Add user in the Database
 router.post('/user',[
 	check('email').isEmail()
-	],user.create);
+	],user.autheticateToken,user.create);
 //check username and password
-router.put('/user/login/:email',user.checkUser);
+router.post('/user/login/',user.checkUser);
 
  // Retrieve all user
- router.get('/user', user.findAll);
+ router.get('/user',user.autheticateToken, user.findAll);
 
  // Retrieve a single User with userEmail
- router.get('/user/:email', user.findOne);
+ router.get('/user/:email',user.autheticateToken, user.findOne);
 
  // Update a user with userEmail
- router.put('/user/:email', user.update);
+ router.put('/user/:email',user.autheticateToken, user.update);
 
  // Delete a user with userEmail
- router.delete('/user/:email', user.delete);
+ router.delete('/user/:email',user.autheticateToken, user.delete);
 
  //Set password with userEmail
- router.put('/user/setpassword/:email',user.updatePassword);
+ router.put('/user/setpassword/:email',user.autheticateToken,user.updatePassword);
 
  router.use(function(err, req, res, next) {
   if (err instanceof ValidationError) {
