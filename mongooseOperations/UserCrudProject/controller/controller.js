811af50927197fabe8e277a28e2c6e9461073008
onkar-josh <onkar.hasabe@joshsoftware.com>
2020-04-01 00:43:54 +0530

    added final changes

--- mongooseOperations/UserCrudProject/controller/controller.js
+++ mongooseOperations/UserCrudProject/controller/controller.js
@@ -5,25 +5,26 @@ const mailSender = require('../email/mailApi');
 const jwt = require('jsonwebtoken');
 exports.create = (req, res) => {
 const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return res.status(422).json({ errors: errors.array() });
-	}
+        if (!errors.isEmpty()) {
+                return res.status(422).json({ errors: errors.array() });
+        }
   if(!req.body.email) {
       return res.status(400).send({
           message: "User email-id can not be empty"
       });
   }
-	const userDetail = new userClass.UserDetails(req.body.email,req.body.name,req.body.mobile_no);
-	let uEmail = userDetail.getUserEmail();
-	let uName = userDetail.getUserName();
-	let uMobileNo = userDetail. getUserMobileNo();
+        const userDetail = new userClass.UserDetails(req.body.email,req.body.name,req.body.mobile_no);
+        let uEmail = userDetail.getUserEmail();
+        let uName = userDetail.getUserName();
+        let uMobileNo = userDetail. getUserMobileNo();
   const user = new User({
       name : uName|| "unknown name",
       email : uEmail ,
+        password : req.body.password,
       location :req.body.location
   });
 
-  user.save()
+user.save()
   .then(data => {
       res.send(data);
   }).catch(err => {
@@ -31,15 +32,15 @@ const errors = validationResult(req);
           message: "email already exist"
       });
   });
-  
-   var mailOptions = {
-	from: 'onkarhasabe1@gmail.com',
-	to: uEmail,
-	subject: 'Testing NodeJs - confirmation of email',
-	cc: 'onkar.hasabe@joshsoftware.com',
-	text: `dear user your email id `+uEmail+` have been successfully registered to generate password contact to developer`
 
-	};
+   var mailOptions = {
+        from: 'onkarhasabe1@gmail.com',
+        to: uEmail,
+        subject: 'Testing NodeJs - confirmation of email',
+        cc: 'onkar.hasabe@joshsoftware.com',
+        text: `dear user your email id `+uEmail+`have been successfully registered  login will provide token which you need to send in header for other APIs`
+
+        };
 
 mailSender.senderFunction(mailOptions);
 };
@@ -48,7 +49,7 @@ exports.findAll = (req, res) => {
   User.find()
   .then(user => {
       res.send(user);
-  }).catch(err => {
+       }).catch(err => {
       res.status(500).send({
           message: err.message || "Some error occurred while retrieving users."
       });
@@ -56,25 +57,25 @@ exports.findAll = (req, res) => {
 };
 
 exports.findOne = (req, res) => {
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return res.status(422).json({ errors: errors.array() });
-	}
-	const userDetail = new userClass.UserDetails(req.params.email);
-	let uEmail = userDetail.getUserEmail();
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+                return res.status(422).json({ errors: errors.array() });
+        }
+        const userDetail = new userClass.UserDetails(req.params.email);
+        let uEmail = userDetail.getUserEmail();
   User.findOne({"email" : uEmail})
   .then(user => {
       if(!user) {
           return res.status(404).send({
               message: "User not found with email " + uEmail
-          });            
+          });
       }
       res.send(user);
   }).catch(err => {
       if(err.kind === 'ObjectId') {
           return res.status(404).send({
               message: "User not found with email " + uEmail
-          });                
+                            });
       }
       return res.status(500).send({
           message: "Error retrieving user with email " + uEmail
@@ -83,25 +84,25 @@ exports.findOne = (req, res) => {
 };
 
 exports.update = (req, res) => {
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return res.status(422).json({ errors: errors.array() });
-	}
- 
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+                return res.status(422).json({ errors: errors.array() });
+        }
+
   if(!req.body.email) {
       return res.status(400).send({
           message: "User email can not be empty"
       });
   }
 
-	const userDetail = new userClass.UserDetails(req.body.email,req.body.name,req.body.mobile_no);
-	let uEmail = userDetail.getUserEmail();
-	let uName = userDetail.getUserName();
-	let uMobileNo = userDetail. getUserMobileNo();
-		
+        const userDetail = new userClass.UserDetails(req.body.email,req.body.name,req.body.mobile_no);
+        let uEmail = userDetail.getUserEmail();
+        let uName = userDetail.getUserName();
+        let uMobileNo = userDetail. getUserMobileNo();
   User.findOneAndUpdate({"email" : req.params.oldemail}, {
       name : uName,
       email : uEmail,
+
       location :req.body.location
   }, {new: true})
   .then(user => {
@@ -115,41 +116,41 @@ exports.update = (req, res) => {
       if(err.kind === 'ObjectId') {
           return res.status(404).send({
               message: "User not found with emailid " + req.params.email
-          });                
+          });
       }
       return res.status(500).send({
           message: "Error updating user with email  id" + req.params.oldemail
       });
   });
-   
+
 };
 
 exports.setUser = (req, res) => {
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return res.status(422).json({ errors: errors.array() });
-	}
- 
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+                return res.status(422).json({ errors: errors.array() });
+        }
+
   if(!req.params.oldemail) {
       return res.status(400).send({
           message: "User email can not be empty"
       });
   }
 
-	const userDetail = new userClass.UserDetails(req.body.email,req.body.name,req.body.mobile_no);
-	let uEmail = userDetail.getUserEmail();
-	let uName = userDetail.getUserName();
-	let uMobileNo = userDetail. getUserMobileNo();
-		
+        const userDetail = new userClass.UserDetails(req.body.email,req.body.name,req.body.mobile_no);
+        let uEmail = userDetail.getUserEmail();
+        let uName = userDetail.getUserName();
+        let uMobileNo = userDetail. getUserMobileNo();
+
   User.findOneAndUpdate({"email" : req.params.oldemail}, {
       name : uName,
-	  password : req.body.password,
+          password : req.body.password,
       location :req.body.location
   }, {new: true})
   .then(user => {
       if(!user) {
           return res.status(404).send({
-              message: "User not found with emailid  " + req.params.email
+                                  message: "User not found with emailid  " + req.params.email
           });
       }
       res.send(user);
@@ -157,26 +158,26 @@ exports.setUser = (req, res) => {
       if(err.kind === 'ObjectId') {
           return res.status(404).send({
               message: "User not found with emailid " + req.params.email
-          });                
+          });
       }
       return res.status(500).send({
           message: "Error updating user with email  id" + req.params.oldemail
       });
   });
-   
+
    var mailOptions = {
-	from: 'onkarhasabe1@gmail.com',
-	to: uEmail,
-	subject: 'confirmation of email',
-	text: `dear user your email id `+uEmail+` have been successfully registered your password is `+req.body.password
-	};
+        from: 'onkarhasabe1@gmail.com',
+        to: uEmail,
+        subject: 'confirmation of email',
+        text: `dear user your email id `+uEmail+` have been successfully registered your password is `+req.body.password
+        };
 
 mailSender.senderFunction(mailOptions);
 };
-
+             
 exports.delete = (req, res) => {
-	const userDetail = new userClass.UserDetails(req.params.email);
-	let uEmail = userDetail.getUserEmail();
+        const userDetail = new userClass.UserDetails(req.params.email);
+        let uEmail = userDetail.getUserEmail();
   User.findOneAndDelete({"email" : uEmail})
   .then(user => {
       if(!user) {
@@ -189,7 +190,7 @@ exports.delete = (req, res) => {
       if(err.kind === 'ObjectId' || err.name === 'NotFound') {
 return res.status(404).send({
               message: "User not found with email " + uEmail
-          });                
+          });
       }
       return res.status(500).send({
           message: "Could not delete user with email " + uEmail
@@ -198,42 +199,43 @@ return res.status(404).send({
 };
 
 exports.login = (req, res) => {
-  User.findOne({email :req.params.email})
+        console.log(req.body.email);
+ User.findOne({email :req.body.email})
   .then(user => {
       if(!user) {
           return res.status(404).send({
-              message: "User not found with email " + req.params.email
-          });            
-      }  
+              message: "User not found with email " + req.body.email
+          });
+      } 
       if((req.body.email == user.email) && (req.body.password == user.password))
       {
-		  var options = {
-			'expiresIn': '1000000'
-		};
-		 const token = jwt.sign({
-			  email : user.email
-		  },
-		  process.env.JWT_KEY,
-		  options
-		  );
+                  var options = {
+                        'expiresIn': process.env.TOKENEXPIRETIME
+                };
+                 const token = jwt.sign({
+                          email : user.email
+                  },
+                  process.env.JWT_KEY,
+                  options
+                  );
           return res.status(200).send({
-              message : "User is valid",
-			  token : token
+              message : "login successful..",
+                          token : token
           });
       }
       else{
           return res.status(404).send({
               message : "User is invalid"
-          });
+});
       }
   }).catch(err => {
       if(err.kind === 'ObjectId') {
           return res.status(404).send({
-              message: "User not found with email " + req.params.email
-          });                
+              message: "User not found with email " + req.body.email
+          });
       }
       return res.status(500).send({
-          message: "Error retrieving user with email " + req.params.email
+          message: "Error retrieving user with email " + req.body.email
       });
   });
 }
@@ -245,3 +247,5 @@ console.log(decoded.header);
 console.log(decoded.payload.email)
   res.json({token : req.params.token})
 };
+
+          
--- mongooseOperations/UserCrudProject/routes/routes.js
+++ mongooseOperations/UserCrudProject/routes/routes.js
@@ -5,44 +5,43 @@ const token = require('../tokenValidation/tokenValidation.js');
 const { check } = require('express-validator');
 
 router.post('/user',[
-	check('email').isEmail(),
-	check('mobile_no').isLength({ min: 10 },
-	)
-	],user.create);
-router.get('/user',token.autheticateToken,user.findAll);
+        check('email').isEmail(),
+        check('mobile_no').isLength({ min: 10 },
+        )
+        ],user.create);
+router.get('/users,token.autheticateToken,user.findAll);
 
  router.get('/user/:email',[
-	check('email').isEmail()
-	], token.autheticateToken,user.findOne);
- 
+        check('email').isEmail()
+        ], token.autheticateToken,user.findOne);
+
  router.put('/user/:oldemail',[
-	check('oldemail').isEmail(),
-	check('email').isEmail(),
-	check('mobile_no').isLength({ min: 10 },
-	)
-	],token.autheticateToken,user.update);
-	
+        check('oldemail').isEmail(),
+        check('email').isEmail(),
+        check('mobile_no').isLength({ min: 10 },
+        )
+        ],token.autheticateToken,user.update);
+
  router.put('/setUser/:oldemail',[
-	check('oldemail').isEmail(),
-	check('mobile_no').isLength({ min: 10 },
+        check('oldemail').isEmail(),
+        check('mobile_no').isLength({ min: 10 },
 	)
-	],user.setUser);
- 
+        ],user.setUser);
+
  router.delete('/user/:email', [
-	check('email').isEmail()
-	],token.autheticateToken,user.delete);
- 
- 
-  router.put('/login/:email',[
-	check('email').isEmail()
-	], user.login);
- 
+        check('email').isEmail()
+        ],token.autheticateToken,user.delete);
+
+
+  router.post('/login',[
+        check('email').isEmail()
+        ], user.login);
+
  router.delete('/user/:email', [
-	check('email').isEmail()
-	],token.autheticateToken,user.delete);
- 
- 
+        check('email').isEmail()
+        ],token.autheticateToken,user.delete);
+
+
  router.get('/getData/:token',user.getData);
 module.exports = router;
-
 
