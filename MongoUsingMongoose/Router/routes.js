5ce724aca0d18f3678a177797443da2dd08fc688
Manish21-v <vitkarmanish21@gmail.com>
2020-03-31 20:59:31 +0530

    added token to the create user api and api name changed

--- MongoUsingMongoose/Router/routes.js
+++ MongoUsingMongoose/Router/routes.js
@@ -2,29 +2,30 @@ const express = require('express');
 const router = express.Router();
 const user = require('../controller/controller');
 const authObj = require('../helperClasses/authentication');
-//const { validate, ValidationError, Joi } = require('express-validation');
 const { check, validationResult } = require('express-validator');
+const cors = require('cors');
+router.use(cors());
 //Add user in the Database
-router.post('/user',[
+router.post('/users',[
 	check('email').isEmail()
 	],authObj.validateToken,user.create);
 //check username and password
-router.post('/user/login/',user.checkUser);
+router.post('/users/login/',user.checkUser);
 
  // Retrieve all user
- router.get('/user',authObj.validateToken,user.findAll);
+ router.get('/users',authObj.validateToken,user.findAll);
 
  // Retrieve a single User with userEmail
- router.get('/user/:email',authObj.validateToken, user.findOne);
+ router.get('/users/:email',authObj.validateToken, user.findOne);
 
  // Update a user with userEmail
- router.put('/user/:email',authObj.validateToken, user.update);
+ router.put('/users/:email',authObj.validateToken, user.update);
 
  // Delete a user with userEmail
- router.delete('/user/:email',authObj.validateToken, user.delete);
+ router.delete('/users/:email',authObj.validateToken, user.delete);
 
  //Set password with userEmail
- router.put('/user/setpassword/:email',authObj.validateToken,user.updatePassword);
+ router.put('/users/setpassword/:email',authObj.validateToken,user.updatePassword);
 
  router.use(function(err, req, res, next) {
   if (err instanceof ValidationError) {
